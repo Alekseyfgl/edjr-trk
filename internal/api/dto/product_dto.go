bf5b13d00e0a30c32d3efeef95b2335f21ad416b
aleksey.fgl <aleksey.fgl@gmail.com>
2025-02-20 11:12:38 +0000

    fix error and validation

--- internal/api/dto/product_dto.go
+++ internal/api/dto/product_dto.go
@@ -10,6 +10,6 @@ type CreateProductRequest struct {
 type PatchProductRequest struct {
 	Title     *string `json:"title" validate:"omitempty,min=3"`
 	Text      *string `json:"text" validate:"omitempty,min=10,max=20000"`
-	ShortText *string `json:"shortText" validate:"required,min=10,max=10000"`
+	ShortText *string `json:"shortText" validate:"omitempty,min=10,max=10000"`
 	Img       *string `json:"img" validate:"omitempty,img_base64_or_null"`
 }
